da3653005e4c9c84c34df8238e8202e13491c647
andrewbeav <andrewbevelhymer@gmail.com>
2017-07-01 21:52:37 -0500

    Finish barebones inmplementation of API

--- public/weathermeme/js/index.js
+++ public/weathermeme/js/index.js
@@ -7,7 +7,10 @@ $(function() {
 });
 
 function updateMeme(position) {
-  $.getJSON('http://andrewbevelhymer.com/weathermeme/api/?owm_appid='+owm_appid+'&lat='+position.coords.latitude+'&lon='+position.coords.longitude, function(data) {
-    console.log('data: ' + data);
+  let apiString = 'http://andrewbevelhymer.com/weathermeme/api/?owm_appid='+owm_appid+'&lat='+position.coords.latitude+'&lon='+position.coords.longitude
+  $.getJSON(apiString, function(data) {
+    console.log(data.meme_location);
+    $('#meme').attr('src', data.meme_location);
+    $('#locationLabel').html(data.weather_info.name);
   });
 }
